6927cc4408174b45cc73734f49b6f2b3ab16e5a2
Coralis <coralis.jpg@gmail.com>
2025-03-13 00:25:23 -0400

    commit

--- index.js
+++
@@ -1,5 +0,0 @@
-import "./styles.css";
-const mainEL = document.querySelector ('main')
-
-mainEL.style.background = "blue"
-mainEL.innerHTML = '<h1>ALAB DOM Manipulation<h2/>';
---
+++ src/index.js
@@ -0,0 +1,48 @@
+import "styles.css";
+
+
+document.addEventListener('DOMContentLoaded'), ()=> {
+const mainEl = document.querySelector('main');
+mainEl.style.backgroundColor = 'var(--main-bg)';
+mainEl.innerHTML = '<h1>DOM Manipulation</h1>';
+mainEl.classList.add('flex-ctr');
+
+const topMenuEl = document.getElementById('top-menu');
+topMenuEl.style.height = '100%';
+topMenuEl.style.backgroundColor = 'var(--top-menu-bg)';
+topMenuEl.classList.add('flex-around');
+
+var menuLinks = [
+    { text: 'about', href: '/about' },
+    { text: 'catalog', href: '/catalog' },
+    { text: 'orders', href: '/orders' },
+    { text: 'account', href: '/account' },
+  ];
+
+    menuLinks.forEach(link => {
+    const menuItem = document.createElement('a');
+    menuItem.setAttribute('href', link.href); 
+    menuItem.textContent = link.text;
+    topMenuEl.appendChild(menuItem);
+  });
+
+};
+
+
+
+// const mainEL = document.querySelector ('main')
+// mainEL.style.background = "blue"
+// mainEL.innerHTML = '<h1>ALAB DOM Manipulation<h2/>';
+
+// const mainEl = document.querySelector('main');
+// mainEl.style.backgroundColor = 'var(--main-bg)';
+// mainEl.innerHTML = '<h1>DOM Manipulation</h1>';
+// mainEl.classList.add('flex-ctr');
+
+
+// const topMenuEl = document.getElementById('top-menu');
+// topMenuEl.style.height = '100%';
+// topMenuEl.style.backgroundColor = 'var(--top-menu-bg)';
+// topMenuEl.classList.add('flex-around');
+
+
